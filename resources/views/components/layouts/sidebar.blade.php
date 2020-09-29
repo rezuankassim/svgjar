08e008905a8033b32d3ceef8d9a280010e1a94e2
Rezuan Kassim <rezuankassim@hotmail.com>
2020-09-29 14:14:36 +0800

    add shadow to sidebar

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
-<div class="min-h-screen bg-white w-64 text-gray-700">
-    <div class="min-h-screen px-6 py-4 flex flex-col justify-between space-y-16">
+<div class="min-h-screen bg-white w-64 shadow-lg text-gray-700">
+    <div class="min-h-screen flex flex-col justify-between shadow-md px-6 py-4  space-y-16">
         <header>
             <a href="{{ route('dashboard') }}" class="flex items-center justify-center space-x-4 text-indigo-600">
                 <svg class="w-8 h-8" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -26,7 +26,7 @@
                         <span>Users</span>
                     </a>
                 </li>
-                
+
                 <li>
                     <a href="#" class="flex items-center space-x-4 rounded-md px-2 py-2 hover:bg-gray-100">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
