107c323bffe9156a748fc2d27a8bc5247b29b059
Rezuan Kassim <rezuankassim@hotmail.com>
2020-09-29 14:12:21 +0800

    initial commit

---
+++ resources/views/components/layouts/sidebar.blade.php
@@ -0,0 +1,48 @@
+<div class="min-h-screen bg-white w-64 text-gray-700">
+    <div class="min-h-screen px-6 py-4 flex flex-col justify-between space-y-16">
+        <header>
+            <a href="{{ route('dashboard') }}" class="flex items-center justify-center space-x-4 text-indigo-600">
+                <svg class="w-8 h-8" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
+                    <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+                </svg>
+            </a>
+        </header>
+        
+        <main class="flex-1">
+            <ul class="flex flex-col space-y-8">
+                <li>
+                    <a href="{{ route('dashboard') }}" class="flex items-center space-x-4 rounded-md px-2 py-2 text-indigo-500 hover:bg-gray-100">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="flex items-center space-x-4 rounded-md px-2 py-2 hover:bg-gray-100">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+
+                        <span>Users</span>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="#" class="flex items-center space-x-4 rounded-md px-2 py-2 hover:bg-gray-100">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path></svg>
+
+                        <span>Icons</span>
+                    </a>
+                </li>
+            </ul>
+        </main>
+
+        <footer>
+            <a href="#" class="flex items-center space-x-4 rounded-md px-2 py-2 hover:bg-gray-100">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+
+                <span>Settings</span>
+            </a>
+        </footer>
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,81 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'hot-pink': '#fd2d78',
+            },
+            rotate: {
+                ...defaultTheme.rotate,
+                '-10': '-10deg',
+                '-9': '-9deg',
+                '-8': '-8deg',
+                '-7': '-7deg',
+                '-6': '-6deg',
+                '-5': '-5deg',
+                '-4': '-4deg',
+                '-3': '-3deg',
+                '-2': '-2deg',
+                '-1': '-1deg',
+                '1': '1deg',
+                '2': '2deg',
+                '3': '3deg',
+                '4': '4deg',
+                '5': '5deg',
+                '6': '6deg',
+                '7': '7deg',
+                '8': '8deg',
+                '9': '9deg',
+                '10': '10deg',
+            },
+            spacing: {
+                ...defaultTheme.spacing,
+                '7': '1.75rem',
+                '9': '2.25rem',
+                '14': '3.5rem',
+                '22': '5.5rem',
+                '26': '6.5rem',
+                '28': '7rem',
+                '30': '7.5rem',
+                '34': '8.5rem',
+                '36': '9rem',
+                '38': '9.5rem',
+                '44': '11rem',
+                '52': '13rem',
+                '60': '15rem',
+                '68': '17rem',
+                '72': '18rem',
+                '76': '19rem',
+                '80': '20rem',
+                '88': '22rem',
+                '96': '24rem',
+                '104': '26rem',
+                '110': '28rem',
+                '118': '30rem',
+                '126': '32rem',
+                '132': '34rem',
+                '140': '36rem',
+            }
+        },
+    },
+
+    variants: {
+        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    },
+
+    plugins: [require('@tailwindcss/ui')],
+};
